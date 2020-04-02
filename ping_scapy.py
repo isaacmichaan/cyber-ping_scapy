81e1ca49925c4147febdd9c8fd5d0f7d2552e873
Isaac Michaan <46087877+isaacmichaan@users.noreply.github.com>
2020-04-03 02:15:11 +0300

    Update ping_scapy.py

--- ping_scapy.py
+++ ping_scapy.py
@@ -3,5 +3,14 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
 ping = Ether()/IP(dst="10.0.2.4")/ICMP()/Raw()
-ping['Raw'].load = "Hello World"
+
+# id
+ping['IP'].id = 2
+
+# filename
+ping['Raw'].load = "file1"
 srp1(ping, timeout = 1)
+
+# message
+ping['Raw'].load = "Hello World"
+a = srp1(ping, timeout = 1)
